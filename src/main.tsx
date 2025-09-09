3c917d86b290ed336becbddb38b906eff24729f4
Artem <skreeetie@gmail.com>
2025-09-09 21:12:26 +0300

    abc

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { store } from "./redux/store/store";
 import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/hh-url">
+      <HashRouter basename="/hh-url">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
